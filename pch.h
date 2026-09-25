42226a034ebd2a8f9bbec6b8dd7cd7a18e2a67cb
NYSkyfox <NYSkyfox@users.noreply.github.com>
2026-09-25 17:17:37 +0000

    fix: include Microsoft.UI.Xaml.Media.h for SolidColorBrush

--- pch.h
+++ pch.h
@@ -17,5 +17,6 @@
 #include <winrt/Microsoft.UI.Xaml.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.Media.h>   // SolidColorBrush 等
 #include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
 #include <winrt/Microsoft.UI.Xaml.Markup.h>
